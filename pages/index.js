d1a25339963a491ce1d1b54505cee6c7a12c1b0c
Vinicios <vorceek@gmail.com>
2023-11-29 00:12:40 +0000

    Atualização `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 function Home() {
   return (
     <div>
-      <h1>Sucesso é o acúmulo de pequenos esforços repetidos dia a dia</h1>;
-      <h2> Página em Construção! Aguarde novas atualizações</h2>
+      <h1>Sucesso é o acúmulo de pequenos esforços repetidos dia a dia</h1>
+      <h2>Página em Construção! Aguarde novas atualizações</h2>
     </div>
   );
 }
