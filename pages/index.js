3b43e6442991325b5f7648aff50eb862dcb6092f
Vorceek <101679184+Vorceek@users.noreply.github.com>
2023-11-29 18:09:30 +0000

    Atualização estilo da Página

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 function Home() {
   return (
     <div>
+      import { style } from pages;
       <h1>Sucesso é o acúmulo de pequenos esforços repetidos dia a dia</h1>
       <h2>Página em Construção! Aguarde novas atualizações</h2>
     </div>
