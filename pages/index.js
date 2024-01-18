785c7b90d99c57a942163030a3e2af50eca6bfe6
Vorceek <101679184+Vorceek@users.noreply.github.com>
2024-01-18 13:47:32 +0000

    New index

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <div>
       <h1>Sucesso é o acúmulo de pequenos esforços repetidos dia a dia</h1>
-      <h2>Página em Construção! Aguarde novas atualizações</h2>
+      <h2>Página em Construção!</h2>
     </div>
   );
 }
